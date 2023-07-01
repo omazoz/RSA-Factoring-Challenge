319103ad595cd5c23f5eabe319d1c99f8bba07eb
omazoz <oumaymamazoz12@gmail.com>
2023-07-01 13:43:51 +0100

    fixed

--- factorization.py
+++ factorization.py
@@ -2,20 +2,20 @@
 import sys
 
 
-def primef(num):
-    if num <= 3:
-        return int(num)
-    if num % 2 == 0:
+def primef(n):
+    if n <= 3:
+        return int(n)
+    if n % 2 == 0:
         return 2
-    elif num % 3 == 0:
+    elif n % 3 == 0:
         return 3
     else:
-        for i in range(5, int(num**0.5) + 1, 6):
-            if num % i == 0:
+        for i in range(5, int(n**0.5) + 1, 6):
+            if n % i == 0:
                 return int(i)
-            if num % (i + 2) == 0:
-                return primef(num/(i+2))
-    return int(num)
+            if n % (i + 2) == 0:
+                return primef(n/(i+2))
+    return int(n)
 
 
 print(primef(int(sys.argv[1])))
